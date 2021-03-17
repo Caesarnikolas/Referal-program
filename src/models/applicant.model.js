0247e57bb16c216a64150de556aecca53590a3cf
IvanShchadov <shchadov.ivan@gmail.com>
2021-03-17 18:38:14 +0300

    Database done

---
+++ src/models/applicant.model.js
@@ -0,0 +1,32 @@
+const { model, Schema } = require("mongoose");
+
+const applicantSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  telegram: {
+    type: String,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  photo: {
+    type: String,
+  },
+  startDate: {
+    type: String,
+  },
+});
+
+const ApplicantModel = model('Applicants', applicantSchema);
+module.exports = ApplicantModel;
