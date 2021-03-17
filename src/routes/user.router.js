7977128c2b3d16341a1d77205955cad6ebe552ae
Aiana <mediaiana@mail.ru>
2021-03-17 18:13:56 +0300

    abc

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,4 +1,28 @@
 const router = require('express').Router();
 
+router.get('/', async (req, res) => {
+  const allStudents = await Student.find();
+  res.render('profile', { allStudents });
+});
 
+router.post('/', async (req, res) => {
+  const {
+    name,
+    email,
+    phone,
+    date,
+  } = req.body;
+  try {
+    const user = await User.create({
+      name,
+      email,
+      phone,
+      date,
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+
+});
 module.exports = router;
