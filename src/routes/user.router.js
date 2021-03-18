38ce9dc56de44ece52f07adc8c8e2edb985c535d
Aiana <mediaiana@mail.ru>
2021-03-18 08:58:20 +0300

    applicant form

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,15 +1,25 @@
 const router = require('express').Router();
 const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
+
+const upload = multer({ dest: 'uploads/' });
 const ApplicantModel = require('../models/applicant.model');
 const UserModel = require('../models/user.model');
+
+const storageConfig = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + file.originalname);
+  }
+});
+router.use(multer({ storage: storageConfig }).single('photo'));
 
 router.get('/', async (req, res) => {
   const allAplicants = await ApplicantModel.find();
   console.log(allAplicants);
   res.render('userPage', { allAplicants });
 });
-
 
 router.post('/', async (req, res) => {
   const {
@@ -18,6 +28,7 @@ router.post('/', async (req, res) => {
     phone,
     date,
   } = req.body;
+  console.log(req.body);
   try {
     const applicant = await ApplicantModel.create({
       name,
@@ -31,7 +42,6 @@ router.post('/', async (req, res) => {
     res.status(200).json(applicant);
   } catch (error) {
     res.sendStatus(500);
-  };
-
+  }
 });
 module.exports = router;
